c494c52dee67fe6857d45e5234cafec26377ce7c
August Dolan <august.dolan1@gmail.com>
2020-09-18 12:34:22 -0700

    build board model & begin rendering view

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,26 @@
+console.log('hello world');
+/*
+  * MODEL
+  * space null if no play yet
+*/
 
+const board = (() => {
+  let board = [];
+  for (let i = 0; i < 3; i++) {
+    board.push([null, null, null])
+  }
+  return board;
+})()
 
+/*
+  * VIEW
+  * renders board to the html page
+*/
+for (let i = 0; i < board.length; i++) {
+  let boardSpace = document.createElement("div")
+  let testContent = document.createTextNode(board[i][0], board[i][1], board[i][2])
+  boardSpace.appendChild(testContent);
+  let testAppend = document.getElementById("board");
+  testAppend.appendChild(boardSpace);
+
+}
